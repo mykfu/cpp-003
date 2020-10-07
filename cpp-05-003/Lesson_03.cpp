b984003f1f75e71a3f21c46779f48cef170ec959
Shamil K <shajdaro@kpfu.ru>
2020-10-07 11:02:11 +0300

    Добавьте файлы проекта.

---
+++ cpp-05-003/Lesson_03.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+	// type name[];
+	// type name[10];
+	// type name[const];
+	// type name[const] = {1, 3};
+	// type name[] = {1, 3};
+
+	const int length = 10;
+	//int anArray[length];
+	int anArray[length] = {9, 8, 7 };
+	int anArray2[length] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+
+	// [0; length) -> [0; length - 1]
+	int a = 1, b = 3, c = 0;
+	//cout << anArray[0] << endl;
+	//cout << anArray[1] << endl;
+	//cout << anArray[2] << endl;
+	//cout << anArray[3] << endl;
+	//cout << anArray[4] << endl;
+
+
+	for (int i = 0; i < length; i++)
+	{
+		cout << anArray[i];
+		if (i != length - 1) cout << ", ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < length; i++)
+	{
+		anArray[i] = i + 1;
+		anArray2[i] = anArray[i];
+		cout << anArray[i];
+		if (i != length - 1) cout << ", ";
+	}
+	cout << endl;
+
+	int arr[length];
+
+	//rand() % 201 - 100 -> [-100; 100]
+
+	srand(time(0));
+
+	int sum = 0;
+	for (int i = 0; i < length; i++)
+	{
+		arr[i] = rand() % 21 - 10;
+		cout << arr[i];
+		if (i != length - 1) cout << ", ";
+
+		sum += arr[i];
+	}
+	cout << endl;
+
+	bool wasPositive = false;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (arr[i] > 0) wasPositive = true;
+	}
+
+	if (wasPositive) {
+		cout << "TRUE\n";
+	}
+	else {
+		cout << "FALSE\n";
+	}
+	
+	int min = arr[0];
+	int index = 0;
+	for (int i = 1; i < length; i++)
+	{
+		if (min > arr[i]) {
+			min = arr[i];
+		}
+	}
+	cout << "min = " << min << endl;
+}
---
+++ cpp-05-003/cpp-05-003.cpp
@@ -0,0 +1,84 @@
+﻿// cpp-05-003.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+//
+///*
+//Lesson 01.
+//
+//@author Shamil Khaydarov
+//
+//*/
+//
+//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//
+//int main()
+//{
+//    setlocale(LC_ALL, "RUS");
+//    cout << "Hello World!\n"; // c++
+//    printf("%s", "Enter string:\n"); // c
+//    
+//    char str[10];
+//
+//    cin >> str; // c++
+//    int k;
+//    scanf_s("%d", &k);
+//    cout << str << endl;
+//
+//
+//    cout << sizeof(int) << endl;
+//    cout << sizeof(short) << endl;
+//    cout << sizeof(short int) << endl;
+//    cout << sizeof(long int) << endl;
+//    cout << sizeof(long long) << endl;
+//    cout << sizeof(long long int) << endl;
+//    
+//    cout << sizeof(float) << endl;
+//    cout << sizeof(double) << endl;
+//
+//    int k = 0, m = 1, n = 10;
+//
+//    int sum = m + n + k; 
+//
+//
+//
+//    int arr[10];
+//    arr[0];
+//
+//    char str[] = "Hello World"; // Hello World\0
+//    char str2[] = {'H', 'e', 'l', 'l', 'o', ' ', '\0'};
+//    cout << str;
+//
+//
+//    k = 3;
+//
+//    k++; // k = k + 1; k+=1
+//    k += 3; // k = k + 3
+//
+//    if (k == 3) { // >, <, >=, <=, !=, ==
+//        cout << "k = three\n";
+//        cout << "kek\n";
+//    }
+//    else if (k > 0 && k < 10) { // && - AND, || - OR, ! - отрицание
+//            cout << "k more than zero\n";
+//    }
+//    else {
+//
+//    }
+//     AND: true && true = true, true && false = false
+//     OR: true || true = true, true || false = true, false || false = false
+//     !true = false
+//
+//
+//}
+//
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+//
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
