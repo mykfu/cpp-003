ceb455d4f04fddd5cadffb178daa68737241c43f
Shamil Khaydarov <hsh0@ya.ru>
2020-11-25 10:01:17 +0300

    add binary search and string compare

--- Game/TestGame.cpp
+++ Game/TestGame.cpp
@@ -52,26 +52,114 @@ bool in_array(T needle, T* haystack, int length) {
     return false;
 }
 
-int binarySearch(int needle, int* haystack, int left, int right) {
-    int middle = (right - left) / 2;
+//[0, 2, 4, 5, 78, 81, 82, 91, 95, 98]
+// 98 ? 78
+// 81, 82, 91, 95, 98
+// 91 ? 98
+//  95, 98
+// 95 ? 98
+// 98 ? 98
+// 98
 
-    if (needle < haystack[middle]) {
+// 2^31-1 + (2^31-1)/2
+// (2^31-1 - (2^31-1)/2)/2
 
+bool binarySearch(int findMe, int* sortedArr, int length) {
+    int left = 0;
+    int right = length - 1;
+
+    // int middle = (left + right) / 2; wrong!
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+
+        if (sortedArr[middle] < findMe) {
+            left = middle + 1;
+        }
+        else if (sortedArr[middle] > findMe) {
+            right = middle - 1;
+        }
+        else {
+            return true;
+        }
     }
-    else if (needle > haystack[middle]) {
 
-    }
-    else {
-        return middle;
-    }
-
+    return false;
 }
 
-bool binarySearch(int needle, int* haystack, int length) {
-    
+// left < right => compareStrings(string left, string right) = -1
+// left == right => compareStrings(string left, string right) = 0
+// left > right => compareStrings(string left, string right) = 1
+int compareStrings(string left, string right) {
+    // left = bfda
+    // right = adsf
+
+    // left = adsf
+    // right = bfds
+
+    // left = adsf
+    // right = adsf
+
+    // left = a
+    // right = adsf
 
 
+    int length = min(left.length(), right.length());
 
+    for (int i = 0; i < length; i++)
+    {
+        unsigned char c1 = left.at(i) * 2;
+        unsigned char c2 = right.at(i) * 2;
+
+        if (left.at(i) == '¸') {
+            c1 = 'å' * 2 + 1;
+        }
+        if (right.at(i) == '¸') {
+            c2 = 'å' * 2 + 1;
+        }
+
+        //if (c1 != c2) {
+        //    return c1 - c2;
+        //}
+
+        if (c1 > c2) {
+            return 1;
+        }
+        else if (c1 < c2) {
+            return -1;
+        }
+    }
+    //return left.length() - right.length();
+    if (left.length() > right.length()) {
+
+        return 1;
+    }
+    else if (left.length() < right.length()) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+bool binarySearch(string findMe, string* sortedArr, int length) {
+    int left = 0;
+    int right = length - 1;
+
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+
+        if (compareStrings(sortedArr[middle], findMe) < 0) {
+            left = middle + 1;
+        }
+        else if (compareStrings(sortedArr[middle], findMe) > 0) {
+            right = middle - 1;
+        }
+        else {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void sortBubble(int*& arr, int length) {
@@ -103,6 +191,7 @@ void printArray(int* arr) {
 #ifdef TEST
 int main()
 {
+    setlocale(LC_ALL, "ru_RU");
     int length = 30;
     int* arr = new int[length];
 
@@ -127,12 +216,26 @@ int main()
     //    cout << (in_array(0, arr, length) ? "true" : "false") << endl;
     //}
 
+    cout << compareStrings("å", "¸");
+
     cout << (in_array(10, arr, length) ? "true" : "false") << endl;
     cout << (in_array(0, arr, length) ? "true" : "false") << endl;
 
-    string* strs = new string[]{ "asdf", "fdsa", "df", "12fd" };
-    cout << (in_array(string("fd"), strs, 4) ? "true" : "false") << endl;
-    cout << (in_array(string("df"), strs, 4) ? "true" : "false") << endl;
+    string* strs = new string[]{ "asdf", "df", "fda", "fdsa" };
+    cout << "search in string " << (in_array(string("fd"), strs, 4) ? "true" : "false") << endl;
+    cout << "search in string " << (in_array(string("df"), strs, 4) ? "true" : "false") << endl;
+
+
+    cout << "binarySearch(10): " << (binarySearch(10, arr, length) ? "true" : "false") << endl;
+    cout << "binarySearch(0): " << (binarySearch(0, arr, length) ? "true" : "false") << endl;
+
+    cout << "binarySearch in string " << (binarySearch(string("fd"), strs, 4) ? "true" : "false") << endl;
+    cout << "binarySearch in string " << (binarySearch(string("df"), strs, 4) ? "true" : "false") << endl;
+    
+
+    string* strs2 = new string[]{ "åå", "å¸", "¸æèê", "ñôûû" };
+    cout << "binarySearch in string " << (binarySearch(string("åå"), strs2, 4) ? "true" : "false") << endl;
+    cout << "binarySearch in string " << (binarySearch(string("¸æèê"), strs2, 4) ? "true" : "false") << endl;
 
 
 
